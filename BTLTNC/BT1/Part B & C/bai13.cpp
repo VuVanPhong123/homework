d308c50e511eb2b6d3335d3a84229d67e8461b7e
VuVanPhong123 <125808502+VuVanPhong123@users.noreply.github.com>
2023-02-21 17:54:26 +0700

    Add files via upload

---
+++ BTLTNC/BT1/Part B & C/bai13.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double x,y;
+	cout<<"nhap toa do cua diem (x,y)"<<endl<<"x = ";
+	cin>>x;
+	cout<<"y = ";
+	cin>>y;
+	cout<<"distance = "<<sqrt(x*x+y*y);
+
+}
